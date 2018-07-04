7506aa62c0324335846cfc8a60cfa12cc5cc825c
GowthamF <40605124+GowthamF@users.noreply.github.com>
2018-07-04 19:17:10 +0530

    offset

--- app/src/main/java/lk/sliit/mad/myapplication/MainActivity.java
+++ app/src/main/java/lk/sliit/mad/myapplication/MainActivity.java
@@ -33,9 +33,10 @@ public class MainActivity extends AppCompatActivity {
         paint.setColor(Color.BLACK);
         paint.setStrokeWidth(10);
         Path p = new Path();
-        p.moveTo(700,800);
-        p.lineTo(700,800);
-        p.lineTo(100,800);
+        p.moveTo(700,900);
+        p.lineTo(-200,900);
+        p.offset(100,250);
+
 
 
         canvas.drawPath(p, paint);
